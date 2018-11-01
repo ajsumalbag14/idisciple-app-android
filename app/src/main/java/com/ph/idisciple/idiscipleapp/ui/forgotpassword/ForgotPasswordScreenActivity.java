240ac28f7ef98c73298d4a68bc3528a82fbb3c75
Charlene Chiang <cchiang@yondu.com>
2018-11-01 23:29:47 +0800

    Updates: > Prepared ForgotPasswordScreenActivity > Updated ui

--- app/src/main/java/com/ph/idisciple/idiscipleapp/ui/forgotpassword/ForgotPasswordScreenActivity.java
+++ app/src/main/java/com/ph/idisciple/idiscipleapp/ui/forgotpassword/ForgotPasswordScreenActivity.java
@@ -1,20 +1,63 @@
 package com.ph.idisciple.idiscipleapp.ui.forgotpassword;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.SpannableString;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.text.style.UnderlineSpan;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.ph.idisciple.idiscipleapp.R;
 import com.ph.idisciple.idiscipleapp.ui.BaseActivity;
 
+import butterknife.BindString;
+import butterknife.BindView;
 import butterknife.OnClick;
 
 public class ForgotPasswordScreenActivity extends BaseActivity implements ForgotPasswordScreenContract.View {
+
+    @BindString(R.string.button_link_back_to_login) String linkBackToLogin;
+    @BindView(R.id.etEmailAddress) EditText etEmailAddress;
+    @BindView(R.id.tvBackToLogin) TextView tvBackToLogin;
+    @BindView(R.id.bEmailResetLink) Button bEmailResetLink;
+
+    @BindView(R.id.tvError) TextView tvError;
+    @BindView(R.id.llConfirmation) LinearLayout llConfirmation;
+    @BindView(R.id.llError) LinearLayout llError;
+
+    @OnClick(R.id.bLogin)
+    public void onLoginClick(){
+
+        String emailAddress = etEmailAddress.getText().toString();
+
+        if(TextUtils.isEmpty(emailAddress)) {
+            llError.setVisibility(View.VISIBLE);
+        } else {
+            if(!Patterns.EMAIL_ADDRESS.matcher(emailAddress).matches()) {
+                llError.setVisibility(View.VISIBLE);
+            } else {
+                updateButtonIfEnabled(false);
+                showLoadingDialog();
+                mPresenter.resetPassword(emailAddress);
+            }
+        }
+
+    }
 
     @OnClick(R.id.tvBackToLogin)
     public void onBackToLoginActivity(){
         onBackPressed();
     }
 
+    private ForgotPasswordScreenContract.Presenter mPresenter;
     @Override
     protected int getLayout() {
         return R.layout.activity_forgot_password;
@@ -24,7 +67,41 @@ public class ForgotPasswordScreenActivity extends BaseActivity implements Forgot
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        mPresenter = new ForgotPasswordScreenPresenter(ForgotPasswordScreenActivity.this, this);
+        updateButtonIfEnabled(checkIfAllRequiredFieldsAreNotEmpty());
 
+        SpannableString content = new SpannableString(linkBackToLogin);
+        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+        tvBackToLogin.setText(content);
+
+        etEmailAddress.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int deletedCharacter, int enteredCharacter) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                updateButtonIfEnabled(checkIfAllRequiredFieldsAreNotEmpty());
+                llError.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+    private boolean checkIfAllRequiredFieldsAreNotEmpty() {
+        return !TextUtils.isEmpty(etEmailAddress.getText().toString());
+    }
+
+    private void updateButtonIfEnabled(boolean isEnabled){
+        hideLoadingDialog();
+        bEmailResetLink.setEnabled(isEnabled);
+        bEmailResetLink.setTextColor(Color.WHITE);
+        bEmailResetLink.setBackgroundResource(isEnabled ? R.drawable.shape_button_green : R.drawable.shape_button_gray);
     }
 
     @Override
@@ -34,21 +111,24 @@ public class ForgotPasswordScreenActivity extends BaseActivity implements Forgot
 
     @Override
     public void onResetPasswordSuccess(boolean isFirstTimeUser, String token) {
-
+        llConfirmation.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void showNoInternetConnection() {
-
+        updateButtonIfEnabled(true);
+        getShowMessageUtil().showOkMessage(getString(R.string.dialog_error_title_no_internet), getString(R.string.dialog_error_message_no_internet));
     }
 
     @Override
     public void showTimeoutError() {
-
+        updateButtonIfEnabled(true);
+        getShowMessageUtil().showOkMessage(getString(R.string.dialog_error_title_timeout), getString(R.string.dialog_error_message_no_internet));
     }
 
     @Override
     public void showGenericError() {
-
+        updateButtonIfEnabled(true);
+        getShowMessageUtil().showOkMessage(getString(R.string.dialog_error_title_generic), getString(R.string.dialog_error_message_generic));
     }
 }
