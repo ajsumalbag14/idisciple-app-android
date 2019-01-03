c8014056ab473cbc978f42c1ce5456ae3ed40b57
Charlene Chiang <cchiang@yondu.com>
2019-01-03 16:15:50 +0800

    Updates: > Fixed unselected and selected BottomNavigationView

--- app/src/main/java/com/ph/idisciple/idiscipleapp/ui/mainappscreen/MainAppScreenActivity.java
+++ app/src/main/java/com/ph/idisciple/idiscipleapp/ui/mainappscreen/MainAppScreenActivity.java
@@ -2,9 +2,9 @@ package com.ph.idisciple.idiscipleapp.ui.mainappscreen;
 
 import android.annotation.SuppressLint;
 import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.bottomnavigation.LabelVisibilityMode;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.widget.BottomNavigationView;
 import android.view.MenuItem;
@@ -14,8 +14,6 @@ import com.ph.idisciple.idiscipleapp.R;
 import com.ph.idisciple.idiscipleapp.ui.BaseActivity;
 import com.ph.idisciple.idiscipleapp.ui.BaseFragment;
 
-import butterknife.BindColor;
-import butterknife.BindDrawable;
 import butterknife.BindView;
 
 public class MainAppScreenActivity extends BaseActivity implements MainAppScreenContract.View, BottomNavigationView.OnNavigationItemSelectedListener {
@@ -23,6 +21,29 @@ public class MainAppScreenActivity extends BaseActivity implements MainAppScreen
     @BindView(R.id.bottomNavigation)
     BottomNavigationViewEx bottomNavigationView;
 
+    /* BottomNavigationViewEx Colors */
+    int[] colorDefault;
+    int[] colorSpeaker;
+    int[] colorWorkshops;
+    int[] colorSchedule;
+    int[] colorCommunity;
+    int[] colorMore;
+    ColorStateList cslDefault;
+    ColorStateList cslSpeaker;
+    ColorStateList cslWorkshops;
+    ColorStateList cslSchedule;
+    ColorStateList cslCommunity;
+    ColorStateList cslMore;
+
+    final int[][] states = new int[][] {
+            new int[] { android.R.attr.state_enabled}, // enabled
+            new int[] { android.R.attr.state_pressed}  // pressed
+    };
+
+    final int[][] statesDefault = new int[][] {
+            new int[] {-android.R.attr.state_enabled}, // disabled
+            new int[] {-android.R.attr.state_checked} // unchecked
+    };
 
     private BaseFragment fragmentActive = null;
     private MainAppScreenContract.Presenter mPresenter;
@@ -36,52 +57,40 @@ public class MainAppScreenActivity extends BaseActivity implements MainAppScreen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mPresenter = new MainAppScreenPresenter(MainAppScreenActivity.this, this);
-
-
-        // Remove Animation & Shifting to BottomNavigationView
-        bottomNavigationView.enableAnimation(false);
-        bottomNavigationView.enableShiftingMode(false);
-        bottomNavigationView.enableItemShiftingMode(false);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(this);
-        bottomNavigationView.setCurrentItem(0);
+        prepareColorStateList();
+        prepareBottomNavigationBar();
     }
 
     @SuppressLint("RestrictedApi")
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        resetBottomNavigationView();
         String name = menuItem.getTitle().toString();
 
         int order = menuItem.getOrder();
         BottomNavigationItemView bottomNavigationItemView = bottomNavigationView.getBottomNavigationItemView(order);
         bottomNavigationItemView.setTag(name);
 
-//        bottomNavigationView.setBackgroundColor(getResources().getColor(R.color.colorText));
-//        int[] colorIcon;
-//        int[] colorText;
-//        ColorStateList cslScheduleIcon;
-//        ColorStateList cslScheduleText;
-
         switch (menuItem.getItemId()) {
+
+            case R.id.menuSpeakers:
+                bottomNavigationItemView.setIconTintList(cslSpeaker);
+                bottomNavigationItemView.setTextColor(cslSpeaker);
+
+//                getFragmentManager().beginTransaction().hide(fragmentActive).show(fragmentTabFavorites).commit();
+//                fragmentActive = fragmentTabFavorites;
+                break;
+            case R.id.menuWorkshops:
+                bottomNavigationItemView.setIconTintList(cslWorkshops);
+                bottomNavigationItemView.setTextColor(cslWorkshops);
+
+//                getFragmentManager().beginTransaction().hide(fragmentActive).show(fragmentTabAccount).commit();
+//                fragmentActive = fragmentTabAccount;
+//                mPresenter.fetchProfile();
+                break;
             case R.id.menuSchedule:
-//                colorIcon = new int[] {
-//                        Color.WHITE,  // enabled
-//                        getResources().getColor(R.color.colorIDiscipleRed), // disabled
-//                        getResources().getColor(R.color.colorIDiscipleRed), // unchecked
-//                        Color.WHITE// pressed
-//                };
-//                cslScheduleIcon = new ColorStateList(new int[][]{new int[0]}, new int[]{colorIcon[0]});
-//                bottomNavigationView.setIconTintList(0, cslScheduleIcon);
-//
-//                colorText = new int[] {
-//                        Color.WHITE,  // enabled
-//                        getResources().getColor(R.color.colorIDiscipleRed), // disabled
-//                        getResources().getColor(R.color.colorIDiscipleRed), // unchecked
-//                        Color.WHITE// pressed
-//                };
-//                cslScheduleText = new ColorStateList(new int[][]{new int[0]}, new int[]{colorText[0]});
-//                bottomNavigationView.setTextTintList(0, cslScheduleText);
-//
+                bottomNavigationItemView.setIconTintList(cslSchedule);
+                bottomNavigationItemView.setTextColor(cslSchedule);
 //                bottomNavigationItemView.setItemBackground(getDrawable(R.drawable.shape_button_red_flat));
 
 //                getFragmentManager().beginTransaction().hide(fragmentActive).show(fragmentTabMap).commit();
@@ -89,20 +98,15 @@ public class MainAppScreenActivity extends BaseActivity implements MainAppScreen
 //                onRefreshButtonClick(); // Refresh every time user selects Map Tab
                 break;
             case R.id.menuCommunity:
+                bottomNavigationItemView.setIconTintList(cslCommunity);
+                bottomNavigationItemView.setTextColor(cslCommunity);
 //                getFragmentManager().beginTransaction().hide(fragmentActive).show(fragmentTabList).commit();
 //                fragmentActive = fragmentTabList;
 //                if(!isSpeechToTextFromMap) onRefreshButtonClick(); // Refresh every time user selects List Tab
                 break;
-            case R.id.menuSpeakers:
-//                getFragmentManager().beginTransaction().hide(fragmentActive).show(fragmentTabFavorites).commit();
-//                fragmentActive = fragmentTabFavorites;
-                break;
-            case R.id.menuWorkshops:
-//                getFragmentManager().beginTransaction().hide(fragmentActive).show(fragmentTabAccount).commit();
-//                fragmentActive = fragmentTabAccount;
-//                mPresenter.fetchProfile();
-                break;
             case R.id.menuMore:
+                bottomNavigationItemView.setIconTintList(cslMore);
+                bottomNavigationItemView.setTextColor(cslMore);
 
                 break;
         }
@@ -110,6 +114,83 @@ public class MainAppScreenActivity extends BaseActivity implements MainAppScreen
 //        refreshedListApplyToTabs();
 
         return false;
+    }
+
+    private void prepareColorStateList(){
+
+        colorDefault = new int[] {
+                getResources().getColor(R.color.colorDividerGray),   // disabled
+                getResources().getColor(R.color.colorDividerGray)   // unchecked
+        };
+
+        colorSpeaker = new int[] {
+                getResources().getColor(R.color.colorIDiscipleOrange), // enabled
+                getResources().getColor(R.color.colorIDiscipleOrange)  // pressed
+        };
+
+        colorWorkshops = new int[] {
+                getResources().getColor(R.color.colorPrimary), // enabled
+                getResources().getColor(R.color.colorPrimary)  // pressed
+        };
+
+        colorSchedule = new int[] {
+                getResources().getColor(R.color.colorIDiscipleRed), // enabled
+                getResources().getColor(R.color.colorIDiscipleRed)  // pressed
+        };
+
+        colorCommunity = new int[] {
+                getResources().getColor(R.color.colorIDiscipleBlue), // enabled
+                getResources().getColor(R.color.colorIDiscipleBlue)  // pressed
+        };
+
+        colorMore = new int[] {
+                getResources().getColor(R.color.colorText),   // enabled
+                getResources().getColor(R.color.colorText)   // pressed
+        };
+
+        cslDefault = new ColorStateList(statesDefault, colorDefault);
+        cslSpeaker = new ColorStateList(states, colorSpeaker);
+        cslCommunity= new ColorStateList(states, colorCommunity);
+        cslSchedule= new ColorStateList(states, colorSchedule);
+        cslWorkshops= new ColorStateList(states, colorWorkshops);
+        cslMore = new ColorStateList(states, colorMore);
+    }
+
+    private void prepareBottomNavigationBar(){
+
+        // Remove Animation & Shifting to BottomNavigationView
+        bottomNavigationView.enableAnimation(false);
+        bottomNavigationView.enableShiftingMode(0, false);
+        bottomNavigationView.enableShiftingMode(1, false);
+        bottomNavigationView.enableShiftingMode(2, false);
+        bottomNavigationView.enableShiftingMode(3, false);
+        bottomNavigationView.enableShiftingMode(4, false);
+        bottomNavigationView.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED);
+        bottomNavigationView.setItemHorizontalTranslationEnabled(false);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        resetBottomNavigationView();
+        bottomNavigationView.setCurrentItem(0);
+
+    }
+
+    private void resetBottomNavigationView(){
+        bottomNavigationView.getMenu().setGroupCheckable(0, false, true); //hack because it won't be unselected
+        bottomNavigationView.setIconTintList(0, cslDefault);
+        bottomNavigationView.setTextTintList(0, cslDefault);
+
+        bottomNavigationView.setIconTintList(1, cslDefault);
+        bottomNavigationView.setTextTintList(1, cslDefault);
+
+        bottomNavigationView.setIconTintList(2, cslDefault);
+        bottomNavigationView.setTextTintList(2, cslDefault);
+
+        bottomNavigationView.setIconTintList(3, cslDefault);
+        bottomNavigationView.setTextTintList(3, cslDefault);
+
+        bottomNavigationView.setIconTintList(4, cslDefault);
+        bottomNavigationView.setTextTintList(4, cslDefault);
     }
 
     @Override
