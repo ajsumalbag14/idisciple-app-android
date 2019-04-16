fdcfcc42bb291917ef8baed21c40e7792084c211
Charlene Chiang <cchiang@yondu.com>
2019-04-16 17:03:33 +0800

    Updates: > Added onBackPressed for hiding List and closing Activity > Added ic_directory images: back, menu and zoom in & out. > Improved activity_view_map  - Added button_directory_back (strings.xml) > Updated buildings.json

--- app/src/main/java/com/ph/idisciple/idiscipleapp/ui/mainappscreen/viewmap/ViewMapActivity.java
+++ app/src/main/java/com/ph/idisciple/idiscipleapp/ui/mainappscreen/viewmap/ViewMapActivity.java
@@ -4,15 +4,15 @@ import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageView;
 
 import com.google.gson.Gson;
-import com.jsibbold.zoomage.ZoomageView;
 import com.ph.idisciple.idiscipleapp.R;
 import com.ph.idisciple.idiscipleapp.data.local.model.JsonBuildingItemRaw;
 import com.ph.idisciple.idiscipleapp.data.local.model.JsonBuildingRawList;
 import com.ph.idisciple.idiscipleapp.ui.BaseActivity;
+import com.ph.idisciple.idiscipleapp.widgets.CustomZoomageView;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -26,12 +26,29 @@ import butterknife.OnClick;
 public class ViewMapActivity extends BaseActivity {
 
     @BindView(R.id.rvList) RecyclerView rvList;
-    @BindView(R.id.ivZoomageView) ZoomageView ivZoomageView;
+    @BindView(R.id.ivZoomageView) CustomZoomageView ivZoomageView;
 
     @OnClick(R.id.ivShowList)
     public void onShowListClick(){
         isListShown = !isListShown;
-        rvList.setVisibility(isListShown ? View.GONE : View.VISIBLE);
+        rvList.setVisibility(isListShown ? View.VISIBLE : View.GONE);
+    }
+
+    @OnClick(R.id.ivBackIcon)
+    public void onBackIconClick(){
+        onBackPressed();
+    }
+
+    @OnClick(R.id.tvBackText)
+    public void onBackTextClick(){
+        onBackPressed();
+    }
+
+    @OnClick(R.id.ivZoomInIcon)
+    public void onZoomInClick(){
+        MotionEvent event = MotionEvent.obtain(0L, 0L, MotionEvent.ACTION_DOWN, 577.17773f, 767.7539f, 0);
+        ivZoomageView.dispatchTouchEvent(event);
+        //MotionEvent { action=ACTION_DOWN, actionButton=0, id[0]=0, x[0]=392.60742, y[0]=668.56934, toolType[0]=TOOL_TYPE_FINGER, buttonState=0, metaState=0, flags=0x0, edgeFlags=0x0, pointerCount=1, historySize=0, eventTime=74267430, downTime=74267430, deviceId=5, source=0x1002 }
     }
 
     private LinearLayoutManager mLinearLayoutManager;
@@ -60,5 +77,13 @@ public class ViewMapActivity extends BaseActivity {
 
         mAdapter = new MapLegendsAdapter(ViewMapActivity.this, mRawData);
         rvList.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(isListShown){
+            onShowListClick();
+        } else
+            finish();
     }
 }
