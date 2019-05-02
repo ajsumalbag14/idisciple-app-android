18703fae30620a05dc57e2739985e462d97ff348
Charlene Chiang <cchiang@yondu.com>
2019-05-02 16:54:00 +0800

    Updates: > Hide Zoom In & Out Buttons (ViewMapActivity) > Clean codes in ViewResourceActivity

--- app/src/main/java/com/ph/idisciple/idiscipleapp/ui/mainappscreen/viewmap/ViewMapActivity.java
+++ app/src/main/java/com/ph/idisciple/idiscipleapp/ui/mainappscreen/viewmap/ViewMapActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -15,7 +14,6 @@ import com.ph.idisciple.idiscipleapp.R;
 import com.ph.idisciple.idiscipleapp.data.local.model.JsonBuildingItemRaw;
 import com.ph.idisciple.idiscipleapp.data.local.model.JsonBuildingRawList;
 import com.ph.idisciple.idiscipleapp.ui.BaseActivity;
-import com.ph.idisciple.idiscipleapp.ui.mainappscreen.MainAppScreenActivity;
 import com.ph.idisciple.idiscipleapp.ui.mainappscreen.communityfragment.yourprofiledialog.YourProfileInfoDialogActivity;
 import com.ph.idisciple.idiscipleapp.widgets.CustomZoomageView;
 
@@ -50,12 +48,12 @@ public class ViewMapActivity extends BaseActivity {
         onBackPressed();
     }
 
-    @OnClick(R.id.ivZoomInIcon)
-    public void onZoomInClick(){
-        MotionEvent event = MotionEvent.obtain(0L, 0L, MotionEvent.ACTION_DOWN, 577.17773f, 767.7539f, 0);
-        ivZoomageView.dispatchTouchEvent(event);
-        //MotionEvent { action=ACTION_DOWN, actionButton=0, id[0]=0, x[0]=392.60742, y[0]=668.56934, toolType[0]=TOOL_TYPE_FINGER, buttonState=0, metaState=0, flags=0x0, edgeFlags=0x0, pointerCount=1, historySize=0, eventTime=74267430, downTime=74267430, deviceId=5, source=0x1002 }
-    }
+//    @OnClick(R.id.ivZoomInIcon)
+//    public void onZoomInClick(){
+//        MotionEvent event = MotionEvent.obtain(0L, 0L, MotionEvent.ACTION_DOWN, 577.17773f, 767.7539f, 0);
+//        ivZoomageView.dispatchTouchEvent(event);
+//        //MotionEvent { action=ACTION_DOWN, actionButton=0, id[0]=0, x[0]=392.60742, y[0]=668.56934, toolType[0]=TOOL_TYPE_FINGER, buttonState=0, metaState=0, flags=0x0, edgeFlags=0x0, pointerCount=1, historySize=0, eventTime=74267430, downTime=74267430, deviceId=5, source=0x1002 }
+//    }
 
     @OnClick(R.id.ivToolbarMenuProfile)
     public void onToolbarProfileClick() {
